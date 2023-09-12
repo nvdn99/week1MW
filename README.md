f126460f497b51a4862da2ff1fe3ec423855ba38
Nicole Van Den Nieuwenhuizen <nicolevandennieuwenhuizen@Nicoles-MacBook-Pro-3.local>
2023-09-12 14:50:22 -0400

    Read Me File

---
+++ README.md
@@ -0,0 +1,4 @@
+Hello World! Project 
+
+This code simply creates a text file containing the common code phrase "Hello World!", the code then 
+reads its content, and then finally deletes the file after its text is fully read.
--- fileOperations.js
+++ fileOperations.js
@@ -1,10 +1,28 @@
 const fs = require('fs');
- 
-// Read a file
-fs.readFile('example.txt', 'utf8', (err, data) => {
+
+// Creates a text file with the intent of producing a simple hello word text response 
+fs.writeFile('example.txt', 'Hello, World!', 'utf8', (err) => {
     if (err) {
-        console.error('Error reading the file:', err);
+        console.error('Error creating the file:', err);
         return;
     }
-    console.log('File content:', data);
+    console.log('File created successfully.');
+
+    // Reads the previous file/ line of code, thus producing the hello world text 
+    fs.readFile('example.txt', 'utf8', (readErr, data) => {
+        if (readErr) {
+            console.error('Error reading the file:', readErr);
+            return;
+        }
+        console.log('File content:', data);
+
+        // Deletes the previous hello world text file 
+        fs.unlink('example.txt', (deleteErr) => {
+            if (deleteErr) {
+                console.error('Error deleting the file:', deleteErr);
+                return;
+            }
+            console.log('File deleted successfully.');
+        });
+    });
 });
