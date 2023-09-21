d47b584d7ad45baa5b47b6bfac4f8dfd323450fb
Nicole Van Den Nieuwenhuizen <nicolevandennieuwenhuizen@Nicoles-MacBook-Pro-3.local>
2023-09-21 18:49:10 -0400

    lab 2.0

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ Hello World! Project
 
 This code simply creates a text file containing the common code phrase "Hello World!", the code then 
 reads its content, and then finally deletes the file after its text is fully read.
+
+
+This code sets up a web server that listens on port 3001. When it receives a web request, it checks if the request is to get a list of users or to add a new user. 
+
+If it's a request to get users, it reads a file ("users.json") containing user data and sends it back as a response. 
+
+If it's a request to add a user, it reads the request data, adds the user to the file, and sends back a success message. 
+
+For any other requests or errors, it responds with appropriate error messages.
--- fileOperations.js
+++ fileOperations.js
@@ -1,28 +1,70 @@
 const fs = require('fs');
+const http = require('http');
+const path = require('path');
 
-// Creates a text file with the intent of producing a simple hello word text response 
-fs.writeFile('example.txt', 'Hello, World!', 'utf8', (err) => {
-    if (err) {
-        console.error('Error creating the file:', err);
-        return;
-    }
-    console.log('File created successfully.');
+const port = 3001;
+const dataFilePath = path.join(__dirname, 'users.json');
 
-    // Reads the previous file/ line of code, thus producing the hello world text 
-    fs.readFile('example.txt', 'utf8', (readErr, data) => {
-        if (readErr) {
-            console.error('Error reading the file:', readErr);
-            return;
-        }
-        console.log('File content:', data);
-
-        // Deletes the previous hello world text file 
-        fs.unlink('example.txt', (deleteErr) => {
-            if (deleteErr) {
-                console.error('Error deleting the file:', deleteErr);
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/api/users') {
+        fs.readFile(dataFilePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error('Error reading data file:', err);
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'Internal Server Error' }));
                 return;
             }
-            console.log('File deleted successfully.');
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(data);
         });
-    });
+    } else if (req.method === 'POST' && req.url === '/api/users') {
+        let requestBody = '';
+
+        req.on('data', (chunk) => {
+            requestBody += chunk;
+        });
+
+        req.on('end', () => {
+            try {
+                const newUser = JSON.parse(requestBody);
+
+                fs.readFile(dataFilePath, 'utf8', (err, data) => {
+                    if (err) {
+                        console.error('Error reading data file:', err);
+                        res.writeHead(500, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify({ error: 'Internal Server Error' }));
+                        return;
+                    }
+
+                    const users = JSON.parse(data);
+                    newUser.id = users.length + 1;
+
+                    users.push(newUser);
+                    fs.writeFile(dataFilePath, JSON.stringify(users, null, 2), 'utf8', (err) => {
+                        if (err) {
+                            console.error('Error writing data file:', err);
+                            res.writeHead(500, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify({ error: 'Internal Server Error' }));
+                            return;
+                        }
+
+                        res.writeHead(201, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify(newUser));
+                    });
+                });
+            } catch (error) {
+                console.error('Error parsing request body:', error);
+                res.writeHead(400, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'Bad Request' }));
+            }
+        });
+    } else {
+ 
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Not Found' }));
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
